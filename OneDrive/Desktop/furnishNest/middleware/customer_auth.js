b999ae13d79888f285133f55dd2fe8c5a851fac3
Sharath <sharathj116@gmail.com>
2023-11-29 15:46:03 +0530

    First Commit

---
+++ OneDrive/Desktop/furnishNest/middleware/customer_auth.js
@@ -0,0 +1,11 @@
+const is_User = (req, res, next) => {
+    console.log("sesssion",req.session)
+    console.log("user",req.session.User)
+    if (req.session && req.session.User) next()
+    else{
+      console.log("not logined")
+    //   return res.redirect("/login")
+    res.json({login:"no"})
+    }
+}
+module.exports = is_User
---
+++ OneDrive/Desktop/furnishNest/middleware/customer_block.js
@@ -0,0 +1,8 @@
+const userModel=require("../model/Customer")
+const is_Blocked=async(req,res,next)=>{
+  if(req.session.is_block)
+      res.render("backEnd/forbidden.ejs")
+  else
+    next()
+}
+module.exports=is_Blocked
